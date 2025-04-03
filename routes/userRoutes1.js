b11819a22f16c1683401b924a3a6494bac50501c
AbhishekVarpe100 <abhishekvarpe8@gmail.com>
2025-04-03 07:16:36 +0530

    initial commit

--- routes/userRoutes1.js
+++ routes/userRoutes1.js
@@ -37,8 +37,8 @@ router.post("/register", registerValidator, userController.register);
 
 
 router.get('/test-route',async(req,res)=>{
-  // const user=await User.find()
-  res.json("Hello")
+  const user=await User.find()
+  res.json(user)
 })
 
 router.post("/login", userController.login);
